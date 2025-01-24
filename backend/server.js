f5f3feb91082ed96fbfe88d3162eb469a95a92cf
Nick-Massagli <mas19015@byui.edu>
2025-01-24 14:06:43 -0700

    Lesson 03

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const professionalRoutes = require('./routes/professional');
+
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
